a114f80bbce37ac11090de167e68e62d12228a37
Mikaela Reyes <reyes.mikaelahelene@gmail.com>
2021-10-05 14:01:37 -0400

    committing before i try more web3 package installs

--- src/App.js
+++ src/App.js
@@ -3,21 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HELLO!!!!!!!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello!</h1>
     </div>
   );
 }
