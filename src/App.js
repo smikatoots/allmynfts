5bb72c41b4daf221baaea74ec10d5a22a823fff5
Mikaela Reyes <reyes.mikaelahelene@gmail.com>
2021-10-06 20:52:17 -0400

    dynamic ad of nfts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useState } from 'react';
 import Web3 from 'web3';
 import './App.css';
 import { Moralis } from 'moralis';
+import { isCompositeComponent } from 'react-dom/test-utils';
 
 //To-Do's
 // - dynamic add of address
@@ -9,49 +10,62 @@ import { Moralis } from 'moralis';
 // - more metadata
 // - incorporate solana NFTs 
 
-async function getNFTs(accountAddress, setNftArray, nftArray) {
-  const options = { chain: 'eth', address: "0xbeeef66749b64afe43bbc9475635eb510cfe4922"};
+async function getNFTs(accountAddress, setNftArray) {
+  console.log("getNFTs renders", accountAddress)
+  const options = { chain: 'eth', address: accountAddress};
   const nfts = await Moralis.Web3.getNFTs(options);
-
-  console.log("TEST ABC, ", nftArray, setNftArray);
-
+  setNftArray([]);
   nfts.forEach(function(nft) {
-    // console.log("each nft ", nft.token_uri);
-    let url = nft.token_uri;
-
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      setNftArray(nftArray => [...nftArray, data.image]);
-      // console.log('data image ', data.image); 
-    });      
-
+    console.log("every nft", nft);
+    if(nft.token_uri!==undefined){
+      console.log("testnftokenuri not undefined");
+      let url = fixURL(nft.token_uri);
+      fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log("data", data);
+        setNftArray(nftArray => [...nftArray, {title: data.name, link: data.external_url, desc: data.description, image: fixURL(data.image)} ]);
+      });  
+    }
   })
-
-  console.log("nfts", nfts);
 }
 
-async function loadBlockchainData(setAccountAddress, setNftArray, nftArray) {
+function fixURL(url) {
+  if(url.startsWith("ipfs://ipfs/")) {
+    return "https://ipfs.moralis.io:2053/ipfs/"+url.split("ipfs://ipfs/").slice(-1)
+  }
+  else if(url.startsWith("ipfs://")) {
+    return "https://ipfs.moralis.io:2053/ipfs/"+url.split("ipfs://").slice(-1)
+  }
+  else {
+    return url+"?format=json"
+  }
+}
+
+async function loadBlockchainData(setAccountAddress, setNftArray, accountAddress) {
     const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
-    const network = await web3.eth.net.getNetworkType();
+    // const network = await web3.eth.net.getNetworkType();
     await window.ethereum.enable();
-    const accounts = await web3.eth.getAccounts();
-    setAccountAddress(accounts[0]);
-    getNFTs(accounts[0], setNftArray, nftArray);
+    // const accounts = await web3.eth.getAccounts();
+    setAccountAddress(accountAddress);
+    getNFTs(accountAddress, setNftArray);
   }
   
 function App() {
-  const [accountAddress, setAccountAddress] = useState("loading");
+  const [accountAddress, setAccountAddress] = useState("input an address");
   const [isConnectionStale, setIsConnectionStale] = useState(false);
-  const [nftArray, setNftArray] = useState(["https://service.cryptotrunks.co/image.png?token=9322"]);
+  const [nftArray, setNftArray] = useState([]);
+  console.log("nftarray", nftArray);
 
-  //useCallBack
-  //useEffect
-  //too many re-renders 
+  const viewEthereumNfts = useCallback(() => {
+    const address = document.getElementById("address").value;
+    console.log(address);
+    loadBlockchainData(setAccountAddress, setNftArray, address);
+  }, [setAccountAddress, setNftArray]);
 
   const connect = useCallback(async () => {
     if (isConnectionStale || accountAddress === "loading") {
-      loadBlockchainData(setAccountAddress, setNftArray, nftArray);
+      loadBlockchainData(setAccountAddress, setNftArray, accountAddress);
       setIsConnectionStale(false);
     };    
   }, [accountAddress, setAccountAddress, isConnectionStale, setIsConnectionStale]);
@@ -67,13 +81,27 @@ function App() {
     }, 100000);
   }, [setIsConnectionStale]);
 
+  const isNftEmpty = nftArray.length === 0
+
   return (
     <div className="container">
       <h1>Hello!</h1>
+      <form> 
+        <input type="text" id="address"/>
+      </form> 
+      <button onClick={viewEthereumNfts}> View NFTs</button> 
       <p>Your account: {accountAddress}</p>
-        {nftArray.map((nfts)=> (
-          <img src={nfts} height="100" width="100"></img>
-        ))}
+        {isNftEmpty && accountAddress !== "loading"
+          ? 
+          <div>No NFTs</div>
+          :
+          nftArray.map((nfts)=> (
+            <div>
+              <h3><a href={nfts.link}>{nfts.title}</a></h3>
+              <p>{nfts.desc}</p>
+              <img src={nfts.image} height="200" width="200"></img>
+            </div> 
+          ))}
     </div>
   );
 }
